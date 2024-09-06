64546385e037ba1813f1a9c322613e0bf86a1295
Ricardo Madrigal <ricmad142@gmail.com>
2024-09-06 00:12:52 -0600

    Practica01

--- Laboratorio/Practica01/Practica01/app1.asm
+++ Laboratorio/Practica01/Practica01/app1.asm
@@ -1,7 +1,7 @@
 ;
 ; Practica01.asm
 ;
-; Created: 26/08/2024 08:51:32 a. m.
+; Created: 05/09/2024 11:27 a.m.
 ; Author : Madris
 ;
 
@@ -9,10 +9,9 @@
 ; Replace with your application code
 start:
 
-	ldi r16,0xff
-	ldi r17, 0xca
+	ldi r16,0xff ; se introduce la constante 0xff b 1111 1111 al r16.
+	ldi r17, 0xaa ; se introduce la constante 0xaa b 1010 1010 al r17.
 	
-	out ddrd, r16
-	out portd, r17
-	fin: rjmp fin
-
+	out ddrb, r16 ;  programamos el puerto d como salida.
+	out portb, r17 ; mostramos lo que esté en el registro 17 en el puerto D
+	fin: rjmp fin ; salto al fin, es un ciclo.
