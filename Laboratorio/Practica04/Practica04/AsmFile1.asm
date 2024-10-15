4a927bc9a9d153475df92dcb5c1ee769ee5017dc
Ricardo Madrigal <ricmad142@gmail.com>
2024-10-14 20:13:28 -0600

    Conclusiones Practica 04

---
+++ Laboratorio/Practica04/Practica04/AsmFile1.asm
@@ -0,0 +1,41 @@
+/*
+ * AsmFile1.asm
+ *
+ *  Created: 14/10/2024 07:49:02 p. m.
+ *   Author: Madris
+ */ 
+
+ start:
+
+	LDI R16,0x3F ; metemos 8 bits en r16 (0011 1111)
+	OUT DDRD,R16 ; programamos como salida (B)
+
+	LDI R16,0x55 ; iniciando cuenta 0x55 PB
+	OUT PORTD,R16 ; nos muestra por puerto B lo que esté en r16
+
+CUENTA:
+	; EMPIEZA RETARDO
+
+	LDI R17,0x00 ; A
+	LDI R18,0x00 ; B
+	LDI R19,0x00 ; C
+	LDI R20,0x04 ; COMPARACIÓN CON C
+	LDI R21,0x00 ; COMPARACIÓN CON A Y B
+
+INCREMENTO:
+	INC R17 ; INCREMENTA R17
+	CPSE R17,R21 ; SE COMPARA R17 CON R21
+	RJMP INCREMENTO ; REGRESA A INCREMENTO
+
+	INC R18 ; INCREMENTA R18
+	CPSE R18,R21 ; SE COMPARA R18 CON R21
+	RJMP INCREMENTO ; REGRESA A INCREMENTO
+
+	INC R19 ; INCREMENTA R19
+	CPSE R19,R20 ; SE COMPARA R19 CON R20
+	RJMP INCREMENTO ; REGRESA A INCREMENTO
+
+	; FIN DEL RETARDO
+	INC R16 ; hace el incremento de cuenta (0x56)
+	OUT PORTD, R16 ; sale por r16
+	RJMP CUENTA ; salto a cuenta para que se haga el ciclo
